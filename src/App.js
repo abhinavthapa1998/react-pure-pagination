6e17c128148c9de3cb79087ca4b66cf28a37738b
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-08 20:23:08 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import "./styles.css";
 import axios from "axios";
+import { useState, useEffect } from "react";
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  const fetchPosts = async () => {
+    setLoading(true);
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setPosts(res.data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
