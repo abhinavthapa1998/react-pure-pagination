24a85831cf69425261b3555c910d01dc6f2b04b9
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-08 20:46:22 +0530

    Pagination Component Start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./styles.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Posts from "../components/Posts";
+import Pagination from "../components/Pagination";
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   const fetchPosts = async () => {
     setLoading(true);
     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
@@ -16,9 +21,10 @@ export default function App() {
     fetchPosts();
   }, []);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
     </div>
   );
 }
